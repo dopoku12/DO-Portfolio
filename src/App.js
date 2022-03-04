af0daec5534b97c56dfaf4e0bde471f79a4eb48c
David Opoku <71300793+dopoku12@users.noreply.github.com>
2022-03-04 14:30:45 -0500

    initilizing starting code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Projects from './Projects'
+import Resume from './Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body>
+
+      <header>
+
+
       </header>
-    </div>
+
+      <Resume />
+
+      <Projects />
+
+      <footer>
+      </footer>
+
+    </body>
   );
 }
 
